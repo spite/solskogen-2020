c585603c0fd55aabdaab2abc3ecabf14cf6cc832
thespite <the.spite@gmail.com>
2020-07-14 22:40:42 +0100

    Added wavefrom renderer Added scale / pan to time marks Added scale / pan to waveform

--- editor/js/SidebarRender.js
+++ editor/js/SidebarRender.js
@@ -2,137 +2,129 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-function SidebarRender( editor ) {
+function SidebarRender(editor) {
+  var signals = editor.signals;
 
-	var signals = editor.signals;
+  var container = new UI.Panel();
+  container.setId("render");
 
-	var container = new UI.Panel();
-	container.setId( 'render' );
+  // Video
 
-	// Video
+  container.add(new UI.Text("Video").setTextTransform("uppercase"));
+  container.add(new UI.Break(), new UI.Break());
 
-	container.add( new UI.Text( 'Video' ).setTextTransform( 'uppercase' ) );
-	container.add( new UI.Break(), new UI.Break() );
+  // Resolution
 
-	// Resolution
+  var resolutionRow = new UI.Row();
+  resolutionRow.add(new UI.Text("Resolution").setWidth("90px"));
 
-	var resolutionRow = new UI.Row();
-	resolutionRow.add( new UI.Text( 'Resolution' ).setWidth( '90px' ) );
+  var videoWidth = new UI.Integer(2304).setWidth("50px");
+  resolutionRow.add(videoWidth);
 
-	var videoWidth = new UI.Integer( 2304 ).setWidth( '50px' );
-	resolutionRow.add( videoWidth );
+  var videoHeight = new UI.Integer(1296).setWidth("50px");
+  resolutionRow.add(videoHeight);
 
-	var videoHeight = new UI.Integer( 1296 ).setWidth( '50px' );
-	resolutionRow.add( videoHeight );
+  container.add(resolutionRow);
 
-	container.add( resolutionRow );
+  // Duration
 
-	// Duration
+  var videoDurationRow = new UI.Row();
+  videoDurationRow.add(new UI.Text("Duration").setWidth("90px"));
 
-	var videoDurationRow = new UI.Row();
-	videoDurationRow.add( new UI.Text( 'Duration' ).setWidth( '90px' ) );
+  var videoDuration = new UI.Integer(93);
+  videoDurationRow.add(videoDuration);
 
-	var videoDuration = new UI.Integer( 93 );
-	videoDurationRow.add( videoDuration );
+  container.add(videoDurationRow);
 
-	container.add( videoDurationRow );
+  // FPS
 
-	// FPS
+  var videoFPSRow = new UI.Row();
+  videoFPSRow.add(new UI.Text("Fps").setWidth("90px"));
 
-	var videoFPSRow = new UI.Row();
-	videoFPSRow.add( new UI.Text( 'Fps' ).setWidth( '90px' ) );
+  var videoFPS = new UI.Integer(60);
+  videoFPSRow.add(videoFPS);
 
-	var videoFPS = new UI.Integer( 60 );
-	videoFPSRow.add( videoFPS );
+  container.add(videoFPSRow);
 
-	container.add( videoFPSRow );
+  // Render
 
-	// Render
+  var render = new UI.Button("RENDER").setMarginLeft("90px");
+  render.onClick(async () => {
+    const player = editor.player;
+    const resources = editor.resources;
 
-	var render = new UI.Button( 'RENDER' ).setMarginLeft( '90px' );
-	render.onClick( async () => {
+    const dom = resources.get("dom");
 
-		const player = editor.player;
-		const resources = editor.resources;
+    // Don't try this at home!
+    window.innerWidth = videoWidth.getValue() / window.devicePixelRatio;
+    window.innerHeight = videoHeight.getValue() / window.devicePixelRatio;
+    window.dispatchEvent(new Event("resize"));
 
-		const dom = resources.get( 'dom' );
+    const element = dom.firstChild;
+    const audio = player.getAudio();
 
-		// Don't try this at home!
-		window.innerWidth = videoWidth.getValue() / window.devicePixelRatio;
-		window.innerHeight = videoHeight.getValue() / window.devicePixelRatio;
-		window.dispatchEvent( new Event( 'resize' ) );
+    //
 
-		const element = dom.firstChild;
-		const audio = player.getAudio();
+    const { createFFmpeg } = FFmpeg;
+    const ffmpeg = createFFmpeg({ log: true });
 
-		//
+    await ffmpeg.load();
 
-		const { createFFmpeg } = FFmpeg;
-		const ffmpeg = createFFmpeg( { log: true } );
+    if (audio !== null) await ffmpeg.write("audio.mp3", audio.src);
 
-		await ffmpeg.load();
+    const fps = videoFPS.getValue();
+    const duration = videoDuration.getValue();
+    const frames = duration * fps;
 
-		if ( audio !== null ) await ffmpeg.write( 'audio.mp3', audio.src );
+    let currentTime = 0;
 
-		const fps = videoFPS.getValue();
-		const duration = videoDuration.getValue();
-		const frames = duration * fps;
+    for (let i = 0; i < frames; i++) {
+      editor.setTime(currentTime);
+      const num = i.toString().padStart(5, "0");
+      await ffmpeg.write(`tmp.${num}.png`, element.toDataURL());
+      currentTime += 1 / fps;
+    }
 
-		let currentTime = 0;
+    if (audio !== null) {
+      await ffmpeg.run(
+        `-framerate ${fps} -pattern_type glob -i *.png -i audio.mp3 -c:a aac -shortest -c:v libx264 -pix_fmt yuv420p -preset slow -crf 0 out.mp4`,
+        { output: "out.mp4" }
+      );
+    } else {
+      await ffmpeg.run(
+        `-framerate ${fps} -pattern_type glob -i *.png -c:v libx264 -pix_fmt yuv420p -preset slow -crf 0 out.mp4`,
+        { output: "out.mp4" }
+      );
+    }
 
-		for ( let i = 0; i < frames; i ++ ) {
+    const data = await ffmpeg.read("out.mp4");
 
-			editor.setTime( currentTime );
-			const num = i.toString().padStart( 5, '0' );
-			await ffmpeg.write( `tmp.${num}.png`, element.toDataURL() );
-			currentTime += 1 / fps;
+    if (audio !== null) await ffmpeg.remove("audio.mp3");
 
-		}
+    for (let i = 0; i < frames; i++) {
+      const num = i.toString().padStart(5, "0");
+      await ffmpeg.remove(`tmp.${num}.png`);
+    }
 
-		if ( audio !== null ) {
+    save(new Blob([data.buffer], { type: "video/mp4" }), "out.mp4");
+  });
+  container.add(render);
 
-			await ffmpeg.run( `-framerate ${fps} -pattern_type glob -i *.png -i audio.mp3 -c:a aac -shortest -c:v libx264 -pix_fmt yuv420p -preset slow -crf 0 out.mp4`, { output: 'out.mp4' });
+  // SAVE
 
-		} else {
+  const link = document.createElement("a");
 
-			await ffmpeg.run( `-framerate ${fps} -pattern_type glob -i *.png -c:v libx264 -pix_fmt yuv420p -preset slow -crf 0 out.mp4`, { output: 'out.mp4' });
+  function save(blob, filename) {
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.dispatchEvent(new MouseEvent("click"));
 
-		}
+    // URL.revokeObjectURL( url ); breaks Firefox...
+  }
 
-		const data = await ffmpeg.read('out.mp4');
+  //
 
-		if ( audio !== null ) await ffmpeg.remove('audio.mp3');
-
-		for ( let i = 0; i < frames; i ++ ) {
-
-			const num = i.toString().padStart( 5, '0' );
-			await ffmpeg.remove( `tmp.${num}.png` );
-
-		}
-
-		save( new Blob( [ data.buffer ], { type: 'video/mp4' } ), 'out.mp4' );
-
-	} );
-	container.add( render );
-
-	// SAVE
-
-	const link = document.createElement( 'a' );
-
-	function save( blob, filename ) {
-
-		link.href = URL.createObjectURL( blob );
-		link.download = filename;
-		link.dispatchEvent( new MouseEvent( 'click' ) );
-
-		// URL.revokeObjectURL( url ); breaks Firefox...
-
-	}
-
-	//
-
-	return container;
-
+  return container;
 }
 
 export { SidebarRender };
--- editor/js/Timeline.js
+++ editor/js/Timeline.js
@@ -2,19 +2,18 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-import { TimelineAnimations } from './TimelineAnimations.js'
+import { TimelineAnimations } from "./TimelineAnimations.js";
 
-function Timeline( editor ) {
+function Timeline(editor) {
+  var signals = editor.signals;
+  var player = editor.player;
 
-	var signals = editor.signals;
-	var player = editor.player;
+  var container = new UI.Panel();
+  container.setId("timeline");
 
-	var container = new UI.Panel();
-	container.setId( 'timeline' );
+  // controls
 
-	// controls
-
-	/*
+  /*
 	var buttons = new UI.Div();
 	buttons.setPosition( 'absolute' );
 	buttons.setTop( '5px' );
@@ -45,197 +44,271 @@ function Timeline( editor ) {
 	buttons.add( button );
 	*/
 
-	// timeline
+  // timeline
 
-	var keysDown = {};
-	document.addEventListener( 'keydown', function ( event ) { keysDown[ event.keyCode ] = true; } );
-	document.addEventListener( 'keyup',   function ( event ) { keysDown[ event.keyCode ] = false; } );
+  var keysDown = {};
+  document.addEventListener("keydown", function (event) {
+    keysDown[event.keyCode] = true;
+  });
+  document.addEventListener("keyup", function (event) {
+    keysDown[event.keyCode] = false;
+  });
 
-	var scale = 32;
-	var prevScale = scale;
+  var scale = 1;
+  var prevScale = scale;
 
-	var timeline = new UI.Panel();
-	timeline.setPosition( 'absolute' );
-	timeline.setTop( '0px' );
-	timeline.setBottom( '0px' );
-	timeline.setWidth( '100%' );
-	timeline.setOverflow( 'hidden' );
-	timeline.dom.addEventListener( 'wheel', function ( event ) {
+  var timeline = new UI.Panel();
+  timeline.setPosition("absolute");
+  timeline.setTop("0px");
+  timeline.setBottom("0px");
+  timeline.setWidth("100%");
+  timeline.setOverflow("hidden");
+  timeline.dom.addEventListener("wheel", function (event) {
+    if (event.altKey === true) {
+      event.preventDefault();
+      scale = Math.min(10, Math.max(0.01, scale + event.deltaY / 10000));
+      signals.timelineScaled.dispatch(scale);
+    }
+  });
+  container.add(timeline);
 
-		if ( event.altKey === true ) {
+  var canvas = document.createElement("canvas");
+  canvas.height = 32;
+  canvas.style.position = "absolute";
+  canvas.addEventListener(
+    "mousedown",
+    function (event) {
+      event.preventDefault();
 
-			event.preventDefault();
+      function onMouseMove(event) {
+        editor.setTime((event.offsetX + scroller.scrollLeft) / (100 * scale));
+      }
 
-			scale = Math.max( 2, scale + ( event.deltaY / 10 ) );
+      function onMouseUp(event) {
+        onMouseMove(event);
 
-			signals.timelineScaled.dispatch( scale );
+        document.removeEventListener("mousemove", onMouseMove);
+        document.removeEventListener("mouseup", onMouseUp);
+      }
 
-		}
+      document.addEventListener("mousemove", onMouseMove, false);
+      document.addEventListener("mouseup", onMouseUp, false);
+    },
+    false
+  );
+  timeline.dom.appendChild(canvas);
 
-	} );
-	container.add( timeline );
+  var waveform = document.createElement("canvas");
+  waveform.style.top = "32px";
+  waveform.style.position = "absolute";
+  timeline.dom.appendChild(waveform);
+  const waveCtx = waveform.getContext("2d");
 
-	var canvas = document.createElement( 'canvas' );
-	canvas.height = 32;
-	canvas.style.position = 'absolute';
-	canvas.addEventListener( 'mousedown', function ( event ) {
+  const request = new XMLHttpRequest();
 
-		event.preventDefault();
+  request.open("GET", "./assets/track.ogg", true);
+  request.responseType = "arraybuffer";
 
-		function onMouseMove( event ) {
+  let waveBuffer;
 
-			editor.setTime( ( event.offsetX + scroller.scrollLeft ) / scale );
+  request.onload = function () {
+    var audioData = request.response;
+    var offlineCtx = new OfflineAudioContext(2, 44100 * 40, 44100);
+    offlineCtx.decodeAudioData(audioData, (buffer) => {
+      waveBuffer = buffer;
+      updateWavefrom();
+    });
+    offlineCtx.startRendering();
+  };
+  request.send();
 
-		}
+  function updateWavefrom() {
+    if (!waveBuffer) {
+      return;
+    }
 
-		function onMouseUp( event ) {
+    const w = scroller.clientWidth;
+    const h = 32;
+    waveform.width = w * window.devicePixelRatio;
+    waveform.height = h * window.devicePixelRatio;
+    waveform.style.width = `${w}px`;
+    waveform.style.height = `${h}px`;
 
-			onMouseMove( event );
+    waveCtx.save();
+    waveCtx.scale(window.devicePixelRatio, window.devicePixelRatio);
 
-			document.removeEventListener( 'mousemove', onMouseMove );
-			document.removeEventListener( 'mouseup', onMouseUp );
+    waveCtx.fillStyle = "#88f";
+    const factor = 441 / scale; // Seems to be related to freq.
+    var data = waveBuffer.getChannelData(0);
+    var step = Math.ceil(factor);
+    const offset = Math.round((scroller.scrollLeft * 44100) / (100 * scale));
+    console.log(scale);
+    var amp = h / 2;
+    for (var i = 0; i < w; i++) {
+      var min = 1.0;
+      var max = -1.0;
+      for (var j = 0; j < step; j++) {
+        var datum = data[offset + i * step + j];
+        if (datum < min) min = datum;
+        if (datum > max) max = datum;
+      }
+      waveCtx.fillRect(i, (1 + min) * amp, 1, Math.max(1, (max - min) * amp));
+    }
 
-		}
+    waveCtx.restore();
+  }
 
-		document.addEventListener( 'mousemove', onMouseMove, false );
-		document.addEventListener( 'mouseup', onMouseUp, false );
+  function updateMarks() {
+    const w = scroller.clientWidth;
+    const h = 32;
+    canvas.width = w * window.devicePixelRatio;
+    canvas.style.width = `${w}px`;
+    canvas.height = h * window.devicePixelRatio;
+    canvas.style.height = `${h}px`;
 
-	}, false );
-	timeline.dom.appendChild( canvas );
+    var context = canvas.getContext("2d", { alpha: false });
+    context.scale(window.devicePixelRatio, window.devicePixelRatio);
 
-	function updateMarks() {
+    context.fillStyle = "#555";
+    context.fillRect(0, 0, w, h);
 
-		canvas.width = scroller.clientWidth;
+    context.strokeStyle = "#888";
+    context.beginPath();
 
-		var context = canvas.getContext( '2d', { alpha: false } );
+    const secondMinWidth = 100;
+    const minWidth = 40;
+    const secondsStep = scale * secondMinWidth;
+    const stride = Math.ceil(minWidth / secondsStep);
+    let stride2 = (stride * secondsStep) / 10;
+    if (stride2 > 20) {
+      stride2 = (stride * secondsStep) / 20;
+    }
 
-		context.fillStyle = '#555';
-		context.fillRect( 0, 0, canvas.width, canvas.height );
+    const start = -scroller.scrollLeft % (scale * secondMinWidth);
+    for (var i = start; i <= w; i += stride * secondsStep) {
+      context.moveTo(i, 18);
+      context.lineTo(i, 26);
 
-		context.strokeStyle = '#888';
-		context.beginPath();
+      for (let j = 0; j < stride * secondsStep; j += stride2) {
+        context.moveTo(i + j, 22);
+        context.lineTo(i + j, 26);
+      }
+    }
 
-		context.translate( - scroller.scrollLeft, 0 );
+    context.stroke();
 
-		var duration = editor.duration;
-		var width = duration * scale;
-		var scale4 = scale / 4;
+    context.font = `${6 * window.devicePixelRatio}px Arial`;
+    context.fillStyle = "#888";
+    context.textAlign = "center";
 
-		for ( var i = 0.5; i <= width; i += scale ) {
+    var step = Math.max(1, Math.floor(64 / scale));
 
-			context.moveTo( i + ( scale4 * 0 ), 18 ); context.lineTo( i + ( scale4 * 0 ), 26 );
+    for (var i = start; i < w; i += stride * secondsStep) {
+      const t = (i + scroller.scrollLeft) / (secondMinWidth * scale);
+      var minute = Math.floor(t / 60);
+      var second = Math.floor(t % 60);
 
-			if ( scale > 16 ) context.moveTo( i + ( scale4 * 1 ), 22 ), context.lineTo( i + ( scale4 * 1 ), 26 );
-			if ( scale >  8 ) context.moveTo( i + ( scale4 * 2 ), 22 ), context.lineTo( i + ( scale4 * 2 ), 26 );
-			if ( scale > 16 ) context.moveTo( i + ( scale4 * 3 ), 22 ), context.lineTo( i + ( scale4 * 3 ), 26 );
+      const minutes = minute > 9 ? minute : `0${minute}`;
+      const seconds = second > 9 ? second : `0${second}`;
+      var text = `${minutes}:${seconds}`;
 
-		}
+      context.fillText(text, i, 13);
+    }
+  }
 
-		context.stroke();
+  var scroller = document.createElement("div");
+  scroller.style.position = "absolute";
+  scroller.style.top = "64px";
+  scroller.style.bottom = "0px";
+  scroller.style.width = "100%";
+  scroller.style.overflow = "auto";
+  scroller.addEventListener(
+    "scroll",
+    function (event) {
+      updateMarks();
+      updateWavefrom();
+      updateTimeMark();
+    },
+    false
+  );
+  timeline.dom.appendChild(scroller);
 
-		context.font = '10px Arial';
-		context.fillStyle = '#888'
-		context.textAlign = 'center';
+  var elements = new TimelineAnimations(editor);
+  scroller.appendChild(elements.dom);
 
-		var step = Math.max( 1, Math.floor( 64 / scale ) );
-
-		for ( var i = 0; i < duration; i += step ) {
-
-			var minute = Math.floor( i / 60 );
-			var second = Math.floor( i % 60 );
-
-			var text = ( minute > 0 ? minute + ':' : '' ) + ( '0' + second ).slice( - 2 );
-
-			context.fillText( text, i * scale, 13 );
-
-		}
-
-	}
-
-	var scroller = document.createElement( 'div' );
-	scroller.style.position = 'absolute';
-	scroller.style.top = '32px';
-	scroller.style.bottom = '0px';
-	scroller.style.width = '100%';
-	scroller.style.overflow = 'auto';
-	scroller.addEventListener( 'scroll', function ( event ) {
-
-		updateMarks();
-		updateTimeMark();
-
-	}, false );
-	timeline.dom.appendChild( scroller );
-
-	var elements = new TimelineAnimations( editor );
-	scroller.appendChild( elements.dom );
-
-	/*
+  /*
 	var curves = new Timeline.Curves( editor );
 	curves.setDisplay( 'none' );
 	scroller.appendChild( curves.dom );
 	*/
 
-	function updateContainers() {
+  function updateContainers() {
+    var width = editor.duration * scale * 100;
 
-		var width = editor.duration * scale;
+    elements.setWidth(width + "px");
+    // curves.setWidth( width + 'px' );
+  }
 
-		elements.setWidth( width + 'px' );
-		// curves.setWidth( width + 'px' );
+  //
 
-	}
+  var loopMark = document.createElement("div");
+  loopMark.style.position = "absolute";
+  loopMark.style.top = 0;
+  loopMark.style.height = 100 + "%";
+  loopMark.style.width = 0;
+  loopMark.style.background = "rgba( 255, 255, 255, 0.1 )";
+  loopMark.style.pointerEvents = "none";
+  loopMark.style.display = "none";
+  timeline.dom.appendChild(loopMark);
 
-	//
+  var timeMark = document.createElement("div");
+  timeMark.style.position = "absolute";
+  timeMark.style.top = "0px";
+  timeMark.style.left = "-8px";
+  timeMark.style.width = "16px";
+  timeMark.style.height = "100%";
+  timeMark.style.background =
+    "linear-gradient(90deg, transparent 8px, #f00 8px, #f00 9px, transparent 9px) 0% 0% / 16px 16px repeat-y";
+  timeMark.style.pointerEvents = "none";
+  timeMark.style.marginTop = "16px";
+  timeMark.appendChild(createTimeMarkImage());
+  timeline.dom.appendChild(timeMark);
 
-	var loopMark = document.createElement( 'div' );
-	loopMark.style.position = 'absolute';
-	loopMark.style.top = 0;
-	loopMark.style.height = 100 + '%';
-	loopMark.style.width = 0;
-	loopMark.style.background = 'rgba( 255, 255, 255, 0.1 )';
-	loopMark.style.pointerEvents = 'none';
-	loopMark.style.display = 'none';
-	timeline.dom.appendChild( loopMark );
+  var timeMarkText = document.createElement("p");
+  timeMarkText.style.position = "absolute";
+  timeMarkText.style.top = "0px";
+  timeMarkText.style.background = "white";
+  timeMarkText.style.padding = "5px";
+  timeMarkText.style.pointerEvents = "none";
+  timeline.dom.appendChild(timeMarkText);
 
-	var timeMark = document.createElement( 'div' );
-	timeMark.style.position = 'absolute';
-	timeMark.style.top = '0px';
-	timeMark.style.left = '-8px';
-	timeMark.style.width = '16px';
-	timeMark.style.height = '100%';
-	timeMark.style.background = 'linear-gradient(90deg, transparent 8px, #f00 8px, #f00 9px, transparent 9px) 0% 0% / 16px 16px repeat-y';
-	timeMark.style.pointerEvents = 'none';
-	timeMark.style.marginTop = '16px';
-	timeMark.appendChild( createTimeMarkImage() );
-	timeline.dom.appendChild( timeMark );
+  function createTimeMarkImage() {
+    var canvas = document.createElement("canvas");
+    canvas.width = 16;
+    canvas.height = 16;
 
-	function createTimeMarkImage() {
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#f00";
+    context.beginPath();
+    context.moveTo(2, 0);
+    context.lineTo(14, 0);
+    context.lineTo(14, 10);
+    context.lineTo(8, 16);
+    context.lineTo(2, 10);
+    context.lineTo(2, 0);
+    context.fill();
 
-		var canvas = document.createElement( 'canvas' );
-		canvas.width = 16;
-		canvas.height = 16;
+    return canvas;
+  }
 
-		var context = canvas.getContext( '2d' );
-		context.fillStyle = '#f00';
-		context.beginPath();
-		context.moveTo( 2, 0 );
-		context.lineTo( 14, 0 );
-		context.lineTo( 14, 10 );
-		context.lineTo( 8, 16 );
-		context.lineTo( 2, 10 );
-		context.lineTo( 2, 0 );
-		context.fill();
+  function updateTimeMark() {
+    var offsetLeft = player.currentTime * scale * 100 - scroller.scrollLeft - 8;
 
-		return canvas;
+    timeMark.style.left = `${offsetLeft}px`;
+    timeMarkText.style.left = `${offsetLeft}px`;
+    timeMarkText.textContent = player.currentTime;
 
-	}
-
-	function updateTimeMark() {
-
-		var offsetLeft = ( player.currentTime * scale ) - scroller.scrollLeft - 8;
-
-		timeMark.style.left = offsetLeft + 'px';
-
-		/*
+    /*
 
 		if ( editor.player.isPlaying ) {
 
@@ -253,58 +326,48 @@ function Timeline( editor ) {
 
 		*/
 
-		// TODO Optimise this
+    // TODO Optimise this
 
-		var loop = player.getLoop();
+    var loop = player.getLoop();
 
-		if ( Array.isArray( loop ) ) {
+    if (Array.isArray(loop)) {
+      var loopStart = loop[0] * scale;
+      var loopEnd = loop[1] * scale;
 
-			var loopStart = loop[ 0 ] * scale;
-			var loopEnd = loop[ 1 ] * scale;
+      loopMark.style.display = "";
+      loopMark.style.left = loopStart - scroller.scrollLeft + "px";
+      loopMark.style.width = loopEnd - loopStart + "px";
+    } else {
+      loopMark.style.display = "none";
+    }
+  }
 
-			loopMark.style.display = '';
-			loopMark.style.left = ( loopStart - scroller.scrollLeft ) + 'px';
-			loopMark.style.width = ( loopEnd - loopStart ) + 'px';
+  // signals
 
-		} else {
+  signals.timeChanged.add(function () {
+    updateTimeMark();
+  });
 
-			loopMark.style.display = 'none';
+  signals.timelineScaled.add(function (value) {
+    scale = value;
 
-		}
+    scroller.scrollLeft = (scroller.scrollLeft * value) / prevScale;
 
-	}
+    updateMarks();
+    updateTimeMark();
+    updateContainers();
+    updateWavefrom();
 
-	// signals
+    prevScale = value;
+  });
 
-	signals.timeChanged.add( function () {
+  signals.windowResized.add(function () {
+    updateMarks();
+    updateContainers();
+    updateWavefrom();
+  });
 
-		updateTimeMark();
-
-	} );
-
-	signals.timelineScaled.add( function ( value ) {
-
-		scale = value;
-
-		scroller.scrollLeft = ( scroller.scrollLeft * value ) / prevScale;
-
-		updateMarks();
-		updateTimeMark();
-		updateContainers();
-
-		prevScale = value;
-
-	} );
-
-	signals.windowResized.add( function () {
-
-		updateMarks();
-		updateContainers();
-
-	} );
-
-	return container;
-
+  return container;
 }
 
 export { Timeline };
